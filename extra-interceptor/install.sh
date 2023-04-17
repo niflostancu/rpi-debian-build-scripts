34eaa22ffe81c433fb2e85ffc9bc9864c3d9572b
Stancu Florin <niflostancu@gmail.com>
2023-04-17 19:00:53 +0300

    interceptor: add systemd network dhcp script

--- extra-interceptor/install.sh
+++ extra-interceptor/install.sh
@@ -5,5 +5,6 @@ SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/" && pwd)"
 
 install -oroot -m755 "$SRC_DIR/config-rtl8367rb.sh" "/usr/local/bin/config-rtl8367rb.sh"
 install -oroot -m644 "$SRC_DIR/udev-interceptor.rules" "/etc/udev/rules.d/90-interceptor.rules"
+install -oroot -m644 "$SRC_DIR/20-wan.network" "/etc/systemd/network/20-wan.network"
 cp "$SRC_DIR/interceptor.dtb" "/boot/interceptor.dtb"
 
