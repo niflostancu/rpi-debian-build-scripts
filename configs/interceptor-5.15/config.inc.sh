d3c74a37bfe6b375dc4c937cf48be1f6dc3e1485
Stancu Florin <niflostancu@gmail.com>
2023-10-28 19:56:31 +0300

    interceptor-5.15: refactor files & script paths
    Provisionable files get copied on rootfs install hook.

--- configs/interceptor-5.15/config.inc.sh
+++ configs/interceptor-5.15/config.inc.sh
@@ -1,28 +1,25 @@
 # Interceptor board builder configuration (for older, 5.15 kernel + swconfig)
 #
 
-KERNEL_PATCHES_DIR="$CUSTOM_CONFIG_DIR/kernel-5.15"
+KERNEL_VERSION=${KERNEL_VERSION:-5.15}
+KERNEL_BRANCH="rpi-$KERNEL_VERSION.y"
+KERNEL_PATCHES_DIR="$CUSTOM_CONFIG_DIR/files/kernel-$KERNEL_VERSION"
 KERNEL_DEFCONFIG=${KERNEL_DEFCONFIG:-"interceptor_defconfig"}
 
 # RPI firmware / boot files
-INTERCEPTOR_DTB=interceptor-rpi-cm4-5.15.dtb
+INTERCEPTOR_DTB=interceptor-rpi-cm4-$KERNEL_VERSION.dtb
 RPI_FIRMWARE_FILES+=(/boot/$INTERCEPTOR_DTB)
 RPI_CONFIG_EXTRA="device_tree=$INTERCEPTOR_DTB"$'\n'
 
 # extra packages to install
 EXTRA_PACKAGES=(firmware-realtek)
 # extra .deb pkgs to install (must be manually placed in `dist/`)
-SWCONFIG_DEB=swconfig-1.0.101-aarch64.deb
-EXTRA_DEBS=($SWCONFIG_DEB)
+SWCONFIG_DEB="$INSTALL_SRC/files/swconfig-1.0.101-aarch64.deb"
+EXTRA_DEBS=("$SWCONFIG_DEB")
 
-# Custom provisioning script that copies built kernel from KERNEL_DEST
-# to the rootfs.
-function custom_provision_script() {
-	true
-}
-
-# Extra installation hook (to run custom scripts)
-function rootfs_extra_script() {
-	source "$CUSTOM_CONFIG_DIR/rootfs-extra-install.sh"
+function rootfs_install_hook() {
+	# copy custom install scripts & files to provisioning dir
+	cp -ar "$CUSTOM_CONFIG_DIR/install-scripts/"* "$INSTALL_SRC/scripts/"
+	cp -ar "$CUSTOM_CONFIG_DIR/files/"* "$INSTALL_SRC/files/"
 }
 
