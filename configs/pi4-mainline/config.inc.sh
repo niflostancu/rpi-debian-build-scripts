2a1f9e293fde773ceb773e3f57e3b633efda3f52
Stancu Florin <niflostancu@gmail.com>
2025-09-06 16:44:49 +0300

    configs: rename, use consistent rpi/mainline kernel suffixes

---
+++ configs/pi4-mainline/config.inc.sh
@@ -0,0 +1,18 @@
+# Debian rootfs + raspberrypi official fork kernel (RPI4 config)
+
+KERNEL_VERSION=${KERNEL_VERSION:-6.12}
+KERNEL_BRANCH="linux-$KERNEL_VERSION.y"
+KERNEL_DEFCONFIG=${KERNEL_DEFCONFIG:-"defconfig"}
+KERNEL_FROM_REPO=n
+
+# add required firmware + some overlays
+RPI_FIRMWARE_FILES=(start4.elf fixup4.dat bcm2711-rpi-4-b.dtb
+	overlays/overlay_map.dtb overlays/hat_map.dtb overlays/upstream-pi4.dtbo
+	overlays/dwc2.dtbo overlays/disable-bt.dtbo)
+
+function rootfs_install_hook() {
+	# copy custom install scripts to exec. dir
+	# cp -ar "$CUSTOM_CONFIG_DIR/install-scripts/"* "$INSTALL_SRC/scripts/"
+	true
+}
+
