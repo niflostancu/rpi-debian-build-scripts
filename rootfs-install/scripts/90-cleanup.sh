ea4c49280f2da1f19d21a14a41da8324093172be
Stancu Florin <niflostancu@gmail.com>
2025-12-18 12:52:05 +0200

    scripts: cleanup: do a minimal sysprep (especially ssh keys)

--- rootfs-install/scripts/90-cleanup.sh
+++ rootfs-install/scripts/90-cleanup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # Cleans up tmp, packages & provisioning files
 
-apt-get --purge -y autoremove
-apt-get -y clean
-
-rm -rf /root/rpi-provisioning/
+(
+	set +e
+	apt-get --purge -y autoremove
+	apt-get -y clean
+	rm -f /etc/ssh/ssh_host_*
+	mv /etc/machine-id{,.old}
+	rm -f /var/lib/systemd/random-seed
+	rm -f /etc/udev/rules.d/70*
+	rm -rf /root/rpi-provisioning/
+	rm -rf /tmp/*
+	rm -rf /var/tmp/* /var/cache/*
+)
 
